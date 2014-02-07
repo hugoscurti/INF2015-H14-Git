90d65d0182735472077ac42e776fcfd6c52ec67b
hugoscurti@gmail.com <hugoscurti@gmail.com>
2014-02-06 22:02:08 -0500

    Ajout d'une ligne dans ProjetGit.java
    ajout d'une librairie en json

--- src/projetgit/ProjetGit.java
+++ src/projetgit/ProjetGit.java
@@ -5,6 +5,7 @@ public class ProjetGit {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub !");
+        System.out.println("Cette ligne a été ajouté par le collaborateur");
     }
     
 }
